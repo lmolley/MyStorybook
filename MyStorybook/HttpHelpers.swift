4b6a1edfa4b70bf95c61cd7d4170aa69f434dbdd
Lauren Molley <lmolley@umich.edu>
2016-02-21 18:57:58 -0500

    added GroPro thumbnail grabbing for after new story is selected

---
+++ MyStorybook/HttpHelpers.swift
@@ -0,0 +1,50 @@
+//
+//  HttpHelpers.swift
+//  MyStorybook
+//
+//  Created by Lauren Molley on 2/21/16.
+//  Copyright © 2016 The My Storybook Team. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+func HTTPsendRequest(request: NSMutableURLRequest,callback: (String, String?) -> Void) {
+    
+    let task = NSURLSession.sharedSession().dataTaskWithRequest(request,completionHandler :
+        {
+            data, response, error in
+            if error != nil {
+                callback("", (error!.localizedDescription) as String)
+            } else {
+                callback(NSString(data: data!, encoding: NSUTF8StringEncoding) as! String,nil)
+            }
+    })
+    
+    task.resume() //Tasks are called with .resume()
+    
+}
+
+func HTTPGet(url: String, callback: (String, String?) -> Void) {
+    let request = NSMutableURLRequest(URL: NSURL(string: url)!) //To get the URL of the receiver , var URL: NSURL? is used
+    HTTPsendRequest(request, callback: callback)
+}
+
+func HTTPsendImageRequest(request: NSMutableURLRequest,callback: (UIImage, String?) -> Void) {
+    
+    let task = NSURLSession.sharedSession().dataTaskWithRequest(request,completionHandler :
+        {
+            data, response, error in
+            if error != nil {
+                callback(UIImage(), (error!.localizedDescription) as String)
+            } else {
+                callback(UIImage(data: data!)!,nil)
+            }
+    })
+    
+    task.resume() //Tasks are called with .resume()
+}
+func HTTPImageGet(url: String, callback: (UIImage, String?) -> Void) {
+    let request = NSMutableURLRequest(URL: NSURL(string: url)!) //To get the URL of the receiver , var URL: NSURL? is used
+    HTTPsendImageRequest(request, callback: callback)
+}
