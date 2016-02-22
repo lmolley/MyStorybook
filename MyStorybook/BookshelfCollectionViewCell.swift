22550cd0a81904809a91a5ef4a9d4a61b62893ed
Rachel Choi <rjcho@0587359243.wireless.umich.net>
2016-02-21 20:20:48 -0500

    added bookshelf

---
+++ MyStorybook/BookshelfCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BookshelfCollectionViewCell.swift
+//  MyStorybook
+//
+//  Created by Rachel Choi on 2/21/16.
+//  Copyright © 2016 The My Storybook Team. All rights reserved.
+//
+
+import UIKit
+
+class BookshelfCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var Cover: UIImageView!
+}
