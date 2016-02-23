1c4de0a72c3571d524fe00959e75d26aac4e0e09
Lauren Molley <lmolley@umich.edu>
2016-02-23 10:23:52 -0500

    adding stuff for photo select

--- MyStorybook/PhotoSelectorViewController.swift
+++ MyStorybook/PhotoSelectorViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+let bottomCell_reuseIdentifier = "bottomSliderCell"
 
-class PhotoSelectorViewController: UIViewController {
+class PhotoSelectorViewController: UICollectionViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     var titleToDisplay:String? = ""
@@ -25,6 +26,33 @@ class PhotoSelectorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = titleToDisplay
+    }
+    
+    
+    
+    
+    //********OVERRIDES FOR COLLECTION VIEW TO WORK***************
+    override func numberOfSectionsInCollectionView(collectionView:
+        UICollectionView!) -> Int {
+            //only one section so we just return 1
+            return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView!,
+        numberOfItemsInSection section: Int) -> Int {
+            //return number of images in our 1 section
+            //            return camera_roll_images.count
+            //            return image_count
+            return (folderToDisplay?.images.count)!
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(bottomCell_reuseIdentifier,forIndexPath: indexPath) as! bottomSliderCell
+        
+        // Configure the cell
+        cell.imageView.image = folderToDisplay.images[indexPath.row]
+        return cell
     }
     
 }
---
+++ MyStorybook/bottomSliderCell.swift
@@ -0,0 +1,13 @@
+//
+//  bottomSliderCell.swift
+//  MyStorybook
+//
+//  Created by Lauren Molley on 2/23/16.
+//  Copyright © 2016 The My Storybook Team. All rights reserved.
+//
+
+import UIKit
+
+class bottomSliderCell : UICollectionViewCell {
+    
+}
