55cfcbbebe358ae630a18752e63266652834742c
Lauren Molley <lmolley@umich.edu>
2016-02-23 17:59:25 -0500

    setting up icon collection view

---
+++ MyStorybook/IconCollectionViewController.swift
@@ -0,0 +1,44 @@
+//
+//  IconCollectionViewController.swift
+//  MyStorybook
+//
+//  Created by Lauren Molley on 2/23/16.
+//  Copyright © 2016 The My Storybook Team. All rights reserved.
+//
+
+import UIKit
+
+let iconCollect_reuseIdentifier = "MyIconCell"
+
+class IconCollectionViewController : UICollectionViewController {
+    var icon_names:[String] = []
+    
+    //********OVERRIDES FOR COLLECTION VIEW TO WORK***************
+    override func numberOfSectionsInCollectionView(collectionView:
+        UICollectionView!) -> Int {
+            //only one section so we just return 1
+            return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView!,
+        numberOfItemsInSection section: Int) -> Int {
+            //return number of images in our 1 section
+            //            return camera_roll_images.count
+            //            return image_count
+            return icon_names.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(iconCollect_reuseIdentifier,forIndexPath: indexPath) as! IconCell
+        
+        // Configure the cell
+        cell.iconView.image = UIImage(named: icon_names[indexPath.row])
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+}
