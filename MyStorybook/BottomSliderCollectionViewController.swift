68faff928f05df081b69b6adf777225f0f112cf3
Lauren Molley <lmolley@umich.edu>
2016-02-23 11:10:29 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 6d99655a711c8584c50a071d436387b5c9afb7d8, reversing
    changes made to 49b5ad22494ab17be9b9e7c14d016198be466dbd.

--- MyStorybook/BottomSliderCollectionViewController.swift
+++ MyStorybook/BottomSliderCollectionViewController.swift
@@ -1,13 +1,12 @@
 //
-//  bottomSliderCell.swift
+//  BottomSliderCollectionViewController.swift
 //  MyStorybook
 //
 //  Created by Lauren Molley on 2/23/16.
 //  Copyright © 2016 The My Storybook Team. All rights reserved.
 //
-
 import UIKit
 
-class bottomSliderCell : UICollectionViewCell {
+class BottomSliderCollectionViewController:UICollectionViewController {
     
 }
--- MyStorybook/PhotoSelectorViewController.swift
+++ MyStorybook/PhotoSelectorViewController.swift
@@ -7,9 +7,8 @@
 //
 
 import UIKit
-let bottomCell_reuseIdentifier = "bottomSliderCell"
 
-class PhotoSelectorViewController: UICollectionViewController {
+class PhotoSelectorViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     var titleToDisplay:String? = ""
@@ -26,33 +25,6 @@ class PhotoSelectorViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = titleToDisplay
-    }
-    
-    
-    
-    
-    //********OVERRIDES FOR COLLECTION VIEW TO WORK***************
-    override func numberOfSectionsInCollectionView(collectionView:
-        UICollectionView!) -> Int {
-            //only one section so we just return 1
-            return 1
-    }
-    
-    override func collectionView(collectionView: UICollectionView!,
-        numberOfItemsInSection section: Int) -> Int {
-            //return number of images in our 1 section
-            //            return camera_roll_images.count
-            //            return image_count
-            return (folderToDisplay?.images.count)!
-    }
-    
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(bottomCell_reuseIdentifier,forIndexPath: indexPath) as! bottomSliderCell
-        
-        // Configure the cell
-        cell.imageView.image = folderToDisplay.images[indexPath.row]
-        return cell
     }
     
 }
