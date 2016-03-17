34f7b0cc80a9039442a6b5d3798884877778670e
David Quesada <davidq2012@gmail.com>
2016-03-16 22:56:27 -0400

    Show images in the story editor.

--- MyStorybook/EditCollectionViewController.swift
+++ MyStorybook/EditCollectionViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 private let edit_reuseIdentifier = "EditCell"
 
@@ -14,30 +15,94 @@ class EditCollectionViewController: UICollectionViewController {
     var selectedIndex = 0
     internal var story: Story?
     
+    var imageThumbnails: [UIImage?]! // In the order as the original storybook.
+    var assets: [PHAsset]! // In the order as the original storybook.
+    
+    var displayedPages: [Int]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupDisplayedPages()
+        loadStoryThumbnails()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    func setupDisplayedPages()
+    {
+        guard let story = story else {
+            fatalError("self.story must be set upon view loading.")
+        }
+        
+        displayedPages = [Int](0 ..< (story.pages?.count ?? 0))
+        
+        guard let pages = story.pages else {
+            return
+        }
+        
+        let ids = pages.map { $0.photoId }
+        let assetOptions = PHFetchOptions()
+        
+        let fetchResult = PHAsset.fetchAssetsWithLocalIdentifiers(ids, options: assetOptions)
+        
+        assets = []
+        
+        for i in 0 ..< fetchResult.count {
+            let asset = fetchResult.objectAtIndex(i) as! PHAsset
+            assets.append(asset)
+        }
+    }
+    
+    func loadStoryThumbnails()
+    {
+        guard let story = story else {
+            fatalError("self.story must be set upon view loading.")
+        }
+        
+        imageThumbnails = Array<UIImage?>(count: story.pages?.count ?? 0, repeatedValue: nil)
     }
     
     // MARK: UICollectionViewDataSource
     
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return story!.pages!.count
+        return displayedPages.count
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(edit_reuseIdentifier, forIndexPath: indexPath) as! EditCollectionViewCell
             
         // Configure the cell
-        let page = self.story!.pages![indexPath.item]
+        let pageIndex = displayedPages[indexPath.item]
         
         cell.EditImage.image = UIImage(named: "default.jpg")
-        cell.EditPageNum.text = "\(page.number)"
+        cell.EditPageNum.text = "\(pageIndex)"
+        
+        if let thumbnail = imageThumbnails[pageIndex] {
+            cell.EditImage.image = thumbnail;
+        } else {
+            let asset = assets[pageIndex]
+            PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: cell.EditImage.frame.size, contentMode: .AspectFit, options: nil, resultHandler: { (image, _) -> Void in
+                guard let image = image else { return }
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.imageThumbnails[pageIndex] = image
+                    collectionView.reloadItemsAtIndexPaths([indexPath])
+                })
+            })
+        }
             
         return cell
     }
