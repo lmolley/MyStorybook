6159ba0c9871de86fe656d563371d5fb33891afe
Lauren Molley <lmolley@umich.edu>
2016-04-12 18:59:30 -0400

    camera control edits

--- MyStorybook/CameraControlViewController.swift
+++ MyStorybook/CameraControlViewController.swift
@@ -195,7 +195,10 @@ class CameraControlViewController : UIViewController {
                     //we know we should've added a photo
                     //so keep trying
                     if cameFromShutter && !madeAnUpdate {
-                        self.startGoProProcessing(true)
+                        
+//                        self.startGoProProcessing(true)
+                        let timer = NSTimer(timeInterval: 1, target: self, selector: "startGoProProcessing", userInfo: nil, repeats: false)
+                        NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
                     }
                     //reenable the shutter button now because we are finished
                     self.shutterButton.enabled = true
@@ -245,23 +248,27 @@ class CameraControlViewController : UIViewController {
             self.currentPhotoBeingSaved = nil
         }
     }
+    
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        updateOrientation()
+    }
    
     func updateOrientation() {
         let orientation: UIDeviceOrientation = UIDevice.currentDevice().orientation
     
         switch (orientation)
         {
-        case .Portrait:
-            selfiePreviewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
-            break
         case .LandscapeRight:
             selfiePreviewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.LandscapeLeft
+//            print(previewView.image!.imageOrientation)
             break
         case .LandscapeLeft:
             selfiePreviewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.LandscapeRight
+//            print(previewView.image!.imageOrientation)
             break
         default:
-            selfiePreviewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            selfiePreviewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.LandscapeLeft
+//            print(previewView.image!.imageOrientation)
             break
         }
         
@@ -279,5 +286,48 @@ extension UIImage {
         UIGraphicsEndImageContext()
         return result
 }
+}
+extension UIImage {
+    public func imageRotatedByDegrees(degrees: CGFloat, flip: Bool) -> UIImage {
+        let radiansToDegrees: (CGFloat) -> CGFloat = {
+            return $0 * (180.0 / CGFloat(M_PI))
+        }
+        let degreesToRadians: (CGFloat) -> CGFloat = {
+            return $0 / 180.0 * CGFloat(M_PI)
+        }
+        
+        // calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox = UIView(frame: CGRect(origin: CGPointZero, size: size))
+        let t = CGAffineTransformMakeRotation(degreesToRadians(degrees));
+        rotatedViewBox.transform = t
+        let rotatedSize = rotatedViewBox.frame.size
+        
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap = UIGraphicsGetCurrentContext()
+        
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        CGContextTranslateCTM(bitmap, rotatedSize.width / 2.0, rotatedSize.height / 2.0);
+        
+        //   // Rotate the image context
+        CGContextRotateCTM(bitmap, degreesToRadians(degrees));
+        
+        // Now, draw the rotated/scaled image into the context
+        var yFlip: CGFloat
+        
+        if(flip){
+            yFlip = CGFloat(-1.0)
+        } else {
+            yFlip = CGFloat(1.0)
+        }
+        
+        CGContextScaleCTM(bitmap, yFlip, -1.0)
+        CGContextDrawImage(bitmap, CGRectMake(-size.width / 2, -size.height / 2, size.width, size.height), CGImage)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
 }
 
